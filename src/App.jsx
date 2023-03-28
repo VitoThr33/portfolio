65c06255fdb60984eee29150f90ff4aad974b534
Ryan McGowan <vitomaserati@gmail.com>
2023-03-27 21:45:46 -0700

    create page sections and scroll snap function, design background in midjourney

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+import Hero from "./components/Hero"
+import Who from "./components/Who"
+import Works from "./components/Works"
+import Contact from "./components/Contact"
+import styled from "styled-components"
+
+const Container = styled.div`
+height: 100vh;
+scroll-snap-type: y mandatory;
+scorll-snap-behavior: smooth;
+overflow-y: auto;
+color: white;
+background: url("./img/bg4.png");
+background-size: cover;
+scrollbar-width: none;
+&::-webkit-scrollbar{
+  display: none;
+}
+`
 function App() {
 
   return (
-    <div>
-      Hello world!
-    </div>
+    <Container>
+      <Hero/>
+      <Who/>
+      <Works/>
+      <Contact/>
+    </Container>
   )
 }
 
